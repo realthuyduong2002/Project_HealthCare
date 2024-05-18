2790b5b9a80d6302556147580ff8bd63f86aeb82
Tarho <20521935@gm.uit.edu.vn>
2024-05-18 23:48:59 +0700

    update again API address
    vào cmd gõ ipconfig.
    dán link wifi address vào folder utils/API.java
    là chạy được

--- Project_HealthCare_FrontEnd/Doctor_FrontEnd/app/src/main/java/com/example/doctorapplication/utils/API.java
+++ Project_HealthCare_FrontEnd/Doctor_FrontEnd/app/src/main/java/com/example/doctorapplication/utils/API.java
@@ -7,7 +7,7 @@ import com.example.doctorapplication.services.PatientService;
 public class API {
     private API(){};
 
-    public static final String API_URL = "http://192.168.1.12:8080/api/";
+    public static final String API_URL = "http://192.168.1.13:8080/api/";
 
     public static PatientService getPatientService(){
        return RetrofitClient.getClient(API_URL).create(PatientService.class);
